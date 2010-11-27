fdf4c2b58ea9df0cfbdc1881dfc3b1e2474c650f
Arnór Barkarson <arnorb06@ru.is>
2010-11-27 21:48:04 +0000

    Accuracy mesurement added

--- NER.pl
+++ NER.pl
@@ -10,7 +10,8 @@
 my $taggedfile = $ARGV[0];
 my $outfile = $ARGV[1];
 my @out;
-
+my $numOfUnKnown = 0;
+my $numOfNPs = 0;
 my $tokenize_command = "perl ../tokeniser.pl ../ex.in ../ex_token.in";
 my $tag_command = "../bin/tree-tagger -token ../english.par ../ex_token.in ../ex.out";
 
@@ -30,7 +31,6 @@ close($taggedfile);
 
 sub npcheck {
 	my $n = $_[0];
-	print "FUCK-----> $n\n";
 	my $result = "(unknown)";
 	if($n =~ /(Sun|Mon|Tues|Wednes|Thurs|Fri|Satur)day/) {
 		$result = "DAY";
@@ -40,6 +40,9 @@ sub npcheck {
 	}
 	elsif($n =~ /(^[A-Z](.)* ([A-Z](.)*son|O'[A-Z](.)*))|((Sir|Lord|Lady|Miss|Mister|Mr|Ms|Mrs|Reverend|Count|Duke|Baron|Earl|Bishop|Emperor|Empress|King|Queen|President|Prime Minister|Dame|Viscount|Marquis|Professor|Dean|Master|Judge|Cardinal|Deacon|Archduke|Abbot|Father|Friar|Sister|Vicar|Chief|Chieftain|Honourable|Right Honourable|Viceroy|CEO|Pontiff|Sheriff|Magistrate|Minister|Barrister|Judicary|Lord Protector|Regent|Private|Constable|Corporal|Sergeant|Lieutinant|Captain|Major|Colonel|Brigadier|General|Marshall|Admiral|Consul|Senator|Chancellor|Ambassador|Doctor|Governor|Governator|Steward|Seneschal|Principal|Officer|Mistress|Madam|Prince|Princess)( [A-Z][. ]*)?)/) {
 		$result = "PERSON";
+	}
+	if($result eq "(unknown)"){
+		$numOfUnKnown++;
 	}
 	return $result;
 }
@@ -57,6 +60,9 @@ sub cdcheck {
 	elsif(@lines[$linesPosCnt + 1] =~ /[dollar(s)?][pound(s)?][euro(s)?][yen(s)?]/){
 		$result = "MONEY";	
 	}
+	if($result eq "(unknown)"){
+		$numOfUnKnown++;
+	}
 	return $result;
 }
 
@@ -68,6 +74,7 @@ for(my $i=0;$i<$#lines+1;++$i) {
 	my $type = "";
 	my $np = $word;
 	if($tag =~ /NP(S)?/) {
+		$numOfNPs++;
 		push(@out, "[ $line[0]");
 		while(1) {
 			my @nextline = split(/\t/,$lines[++$i]);
@@ -78,6 +85,7 @@ for(my $i=0;$i<$#lines+1;++$i) {
 			#print $nexttag;
 			#print "$nextword\n";
 			if($nexttag =~ /NP(S)?/) {
+				$numOfNPs++;
 				$np = $np." $nextword";
 				#print "----> This is np : ".$np;
 				chomp($lines[$i]);
@@ -103,7 +111,12 @@ flock(OFILE, LOCK_EX);
 seek(OFILE, 0, SEEK_SET);
  
 foreach (@out) {
-	#print $_;
+	print $_;
 	print OFILE $_;
 }
+
+print "NUMBER OF NP's : $numOfNPs\n";
+print "NUMERR OF UNKNOWN : $numOfUnKnown\n";
+my $hitrate = ($numOfNPs - $numOfUnKnown)/$numOfNPs;
+print "HITRATE = $hitrate\n";
 close($outfile);
