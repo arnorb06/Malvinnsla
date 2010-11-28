efcdd5b2c4277d781e6dc5e29aa371309246ff33
Arnór Barkarson <arnorb06@ru.is>
2010-11-28 17:39:49 +0000

    FUCK

--- NER.pl
+++ NER.pl
@@ -13,8 +13,8 @@ my $logfile = 'log.txt';
 my @out;
 my $numOfUnKnown = 0;
 my $numOfNPs = 0;
-my $tokenize_command = "perl ../tokeniser.pl ../ex.in ../ex_token.in";
-my $tag_command = "../bin/tree-tagger -token ../english.par ../ex_token.in ../ex.out";
+my $tokenize_command = "perl ../tokeniser.pl ../corpus.in ../ex_token.in";
+my $tag_command = "../bin/tree-tagger -token ../english.par ../ex_token.in ../corpus.out";
 
 system($tokenize_command);
 system($tag_command);
@@ -68,6 +68,7 @@ sub npcontext {
 	my $nextword = $nextline[0];
 	if(( $prevword =~ /^[Ss](aid|ays)$/ ) or ($nextword =~ /^[Ss](aid|ays)$/)) {
 		$result = "PERSON";
+		print "-----------------------------------> FUCK";
 	}
 	while($index<$#lines+1) {		
 		chomp(my @line = split(/\s+/,$lines[$index]));
